5a67ddbfc17954bcce41e1d5cc001f369a35d28e
amingilani <amin@gilani.me>
2016-11-23 17:13:14 +0500

    Solved 2

--- ctci/2-making-anagrams.rb
+++ ctci/2-making-anagrams.rb
@@ -9,20 +9,8 @@ b = 'abc'
 ans = 4
 
 def ana(a, b)
-  a = a.chars.sort
-  b = b.chars.sort
-
-  intersection = a.uniq & b.uniq
-  union = a.uniq | b.uniq
-  difference = intersection - union
-
-  difference_count = difference.map { |e| a.count(e) + b.count(e) }.reduce(:+)
-  union_difference_count = union.map { |e| (a.count(e) - b.count(e)).abs }.reduce(:+)
-
-  # puts union.to_s
-  # puts intersection.to_s
-
-  difference_count + union_difference_count
+  intersection = (a.chars | b.chars).flat_map{|e| [e] * [a.count(e), b.count(e)].min}
+  a.length + b.length - 2 * intersection.length
 end
 
 puts ana(a, b)
