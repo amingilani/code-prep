586a8a443e502f2f3dcca88e1b5fd66d2f4d24b4
amingilani <amin@gilani.me>
2016-11-23 21:28:27 +0500

    Add solution for fifth problem

--- ctci/5-balanced-brackets.rb
+++ ctci/5-balanced-brackets.rb
@@ -6,4 +6,27 @@
 # end
 
 t = 3
-expressions = ['{[()]}', '{[(])}', '{{[[(())]]}}']
+expressions = [
+  '}][}}(}][))]',
+  '[](){()}',
+  '()',
+  '({}([][]))[]()',
+  '{)[](}]}]}))}(())('
+]
+
+def solution(s)
+  until s.empty?
+    start = s.dup
+    s.sub!('()', '')
+    s.sub!('[]', '')
+    s.sub!('{}', '')
+    return 'NO' if start == s
+  end
+  'YES'
+end
+
+expressions.each do |e|
+  puts solution(e)
+end
+
+# puts solution(expressions[3])
